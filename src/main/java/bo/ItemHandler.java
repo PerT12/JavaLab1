4bf54517a164de3f7d44954350ed10c291a5af7c
PerT <per.t.skola@hotmail.com>
2023-10-06 20:35:15 +0200

    update

--- src/main/java/bo/ItemHandler.java
+++ src/main/java/bo/ItemHandler.java
@@ -12,7 +12,7 @@ public class ItemHandler {
         ArrayList<ItemInfo> items = new ArrayList<ItemInfo>();
         for(Iterator it = c .iterator(); it.hasNext();) {
             Item item = (Item) it.next();
-            items.add(new ItemInfo(item.getName(), item.getDescription()));
+            items.add(new ItemInfo(item.getId(), item.getName(), item.getDescription()));
         }
         return items;
     }
